a3608fdff39af9c07439aea9bd39c2f4f4ca3bd6
stallone-dev <stallone.developer@gmail.com>
2024-11-08 17:04:43 -0300

    feat(logger): Config logtape and create basic schemas

--- config/logger.schema.ts
+++ config/logger.schema.ts
@@ -1,1 +1,20 @@
 // Create by Stallone L. de Souza (@stallone-dev) - 2024 - License: CC0 1.0 Universal
+
+import { getLogger } from "@logtape";
+
+export { databaseLogger, internalLogger, routeLogger };
+
+/**
+ * Logger para verificação das rotas
+ */
+const routeLogger = getLogger(["route"]);
+
+/**
+ * Logger para debug interno
+ */
+const internalLogger = getLogger(["internal"]);
+
+/**
+ * Logger para manuseio do DB
+ */
+const databaseLogger = getLogger(["database"]);
--- config/logtape.config.ts
+++ config/logtape.config.ts
@@ -1,1 +1,39 @@
 // Create by Stallone L. de Souza (@stallone-dev) - 2024 - License: CC0 1.0 Universal
+
+import { configure, getConsoleSink, getFileSink } from "@logtape";
+import { database } from "~database";
+
+/**
+ * Comando global de configuração do LogTape,
+ * @see {@link https://logtape.org/manual/config|Configs LogTape} para mais detalhes
+ */
+export default async (): Promise<void> =>
+        await configure({
+                // Formas de controle do fluxo de logs,
+                // Mais detalhes: https://logtape.org/manual/sinks
+                sinks: {
+                        console: getConsoleSink(),
+                        internalFile: getFileSink("./log/internal-log.log"), // Função para salvar em arquivo local
+                        saveOnDB: async (record) => await database.saveLog(record), // Implementação para salvar no database
+                },
+
+                // Listiners de monitoramento dos logs
+                // Mais detalhes: https://logtape.org/manual/categories
+                loggers: [
+                        {
+                                category: ["route"],
+                                level: "info",
+                                sinks: ["console", "saveOnDB"],
+                        },
+                        {
+                                category: ["internal"],
+                                level: "debug",
+                                sinks: ["console", "internalFile"],
+                        },
+                        {
+                                category: ["database"],
+                                level: "debug",
+                                sinks: ["console"],
+                        },
+                ],
+        });
