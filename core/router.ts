baf656781e006f16d84729bc4aa30697d9185736
stallone-dev <stallone.developer@gmail.com>
2024-11-08 13:56:30 -0300

    feat(router): Add routes

--- core/router.ts
+++ core/router.ts
@@ -1,11 +1,15 @@
 // Create by Stallone L. de Souza (@stallone-dev) - 2024 - License: CC0 1.0 Universal
 
 import { Router } from "@oak";
+import { basicRoute } from "~route/basic.route.ts";
+import { fooRoute } from "~route/foo.route.ts";
+import { barRoute } from "~route/bar.route.ts";
+import { getUserLogRoute } from "~route/get-log.route.ts";
 
 export { mainRoute };
 
 const mainRoute = new Router()
-        .get("/", () => {})
-        .post("/foo", () => {})
-        .post("/bar", () => {})
-        .get("/getlog", () => {});
+        .get("/", basicRoute)
+        .post("/foo", fooRoute)
+        .post("/bar", barRoute)
+        .get("/getlog", getUserLogRoute);
